fac3bbc3162df55fe7626acc61339206bd54d6ee
Chris Carter <carterce1997@gmail.com>
2024-07-03 14:57:17 -0400

    clean up some changes that have been sitting here a long time

--- bm.sh
+++ bm.sh
@@ -1,21 +1,35 @@
 #!/bin/sh
 
-mkdir -p $HOME/.local/share/bm
+mkdir -p "$HOME"/.local/share/bm
+BOOKMARKS="$HOME"/.local/share/bm/bookmarks
+touch "$BOOKMARKS"
 
-BOOKMARKS=$HOME/.local/share/bm/bookmarks
-touch $BOOKMARKS
+CACHEDIR="$HOME"/.cache/bm
+mkdir -p "$CACHEDIR"
 
-MENU='dmenu -i -l 50'
+MENU='dmenu -i -nb #191724 -nf #908caa -sb #1f1d2e -sf #e0def4 -l 30'
 
 save_bookmark () {
-	if grep -Fxq "$1" $BOOKMARKS
+	if cat "$BOOKMARKS" | cut -f 1 | grep -Fxq "$1"
 	then
 		echo 'bookmark already exists';
 	elif [ -z "$1" ]
 	then
 		echo 'bookmark is empty'
 	else
-		echo "$1" >> $BOOKMARKS;
+		tempfile="$CACHEDIR"/.bm.txt
+		touch "$tempfile"
+		echo "" >> "$tempfile"
+		echo "# Enter a description for bookmark $1. Lines beginning with # will be ignored." >> "$tempfile"
+		st -e vim "$tempfile"
+		cleaned_entry=$(cat "$tempfile" | sed -e '/^#/d' | tr '\n' ' ' | tr -s ' ')
+		if [ -z "$cleaned_entry" ]
+		then
+			echo -e "$1 " >> "$BOOKMARKS";
+		else
+			echo -e "$1 # "$cleaned_entry"" >> "$BOOKMARKS";
+		fi
+		rm "$tempfile"
 		echo 'bookmarked' "$1"	
 	fi
 }
@@ -23,7 +37,7 @@ save_bookmark () {
 case "$1" in 
 	-e)
 	# edit bookmark file
-	vim $BOOKMARKS
+	vim "$BOOKMARKS"
 	;;
 
 	-c)
@@ -33,8 +47,7 @@ case "$1" in
 
 	-f)
 	# save a file
-	options="$(echo $PWD) $(ls -d $PWD/*)"
-	save_bookmark "$($options | $MENU)"
+	save_bookmark "$(ls -d "$PWD"/* | $MENU)"
 	;;
 
 	-s)
@@ -44,13 +57,13 @@ case "$1" in
 
 	-l)
 	# list bookmarks in terminal
-	cat $BOOKMARKS
+	less -F "$BOOKMARKS"
 	;;
 
 	-t)
 	# type a bookmark with dmenu
-	bookmark="$(cat $BOOKMARKS | $MENU)"
-	xdotool type "$( echo $bookmark | cut -d " " -f 1)"
+	bookmark="$(cat "$BOOKMARKS" | $MENU)"
+	xdotool type "$( echo "$bookmark" | cut -f 1 -d' ')"
 	;;
 
 	*)
